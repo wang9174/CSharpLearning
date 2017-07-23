6696703e41edde5ba0cc6f89a96a8481a82b83d8
wang9174 <775242150@qq.com>
2017-07-23 19:49:54 +0800

    控制台应用程序示例代码
    第三章变量与表达式：控制台应用程序示例代码

---
+++ ConsoleApplication1.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApplication2
+{
+   class Program
+   {
+     static void Main(string[] args)
+     {
+       //添加代码的地方
+       Console.WriteLine("Hello World!");
+       Console.ReadKey();
+     }
+   }
+}
