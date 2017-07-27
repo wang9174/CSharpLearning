0dfda8062ae96b92dddb640d5f791d2a62d6fd67
wang9174 <775242150@qq.com>
2017-07-27 15:34:49 +0800

    上传VS工程文件
    上传ConsoleApplication1，后续代码修改将在工程文件中进行

---
+++ ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//声明区
+
+
+namespace ConsoleApplication1
+{
+    //枚举，结构定义区
+    class Program
+    {
+        //定义函数区
+        static void Main(string[] args)
+        {
+            //添加代码的地方
+            #region 
+            //Console.WriteLine("Hello World!");
+            //Console.ReadKey();
+
+
+            //int a = 12;
+            //int b = 23;
+            //Console.WriteLine("12+23=35");
+            //Console.WriteLine("{0}+{1}={2}", a, b, a+b);
+            //Console.WriteLine($"{a}+{b}={a + b}");
+            //Console.ReadKey();
+            #endregion
+
+            //int myInteger;
+            //string myString;
+            //myInteger = 666;
+            //myString = "\"myIneger\" is ";
+            //Console.WriteLine($"{ myString}{ myInteger}");
+            //Console.ReadKey();
+
+            string myVal = "wang";
+            bool myString;
+            myString = myVal =="Wang";
+            Console.WriteLine(myString );
+            Console.ReadKey();
+        }
+    }
+}
